79b83c139baa5f85d91d3bafa7c3a4270b966a58
Madhumita <madhumitasri24@gmail.com>
2026-03-03 07:06:00 +0530

    "Font change"

--- software_lab/lib/core/theme/app_theme.dart
+++ software_lab/lib/core/theme/app_theme.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import '../constants/app_colors.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   AppTheme._();
 
   static ThemeData lightTheme = ThemeData(
+     textTheme: GoogleFonts.beVietnamProTextTheme(),
     useMaterial3: true,
 
     primaryColor: AppColors.primary,
@@ -16,6 +18,5 @@ class AppTheme {
       tertiary: AppColors.tertiary,
     ),
 
-    fontFamily: 'BeVietnam',
   );
 }
